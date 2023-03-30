77bc4d4155ec5d2dc62369fdd693dc93539dfbf1
BlaD200 <blad600@gmail.com>
2023-03-31 01:24:37 +0300

    Init queue messaging server

---
+++ queueServer/src/main/java/com/vsynytsyn/queueserver/QueueServerApplication.java
@@ -0,0 +1,13 @@
+package com.vsynytsyn.queueserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueueServerApplication.class, args);
+    }
+
+}
