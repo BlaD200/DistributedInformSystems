6820e9fd2d02e4f3a104e451bb72317ead090a24
Danylo Nedosieka <darituan8@gmail.com>
2023-04-07 00:26:40 +0300

    Add scheduler to emitter and fix consumer

--- queueClient/log_emitter.py
+++ queueClient/log_emitter.py
@@ -1,26 +1,44 @@
 import pika
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 import datetime
+import random
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
 
-# Info or Debug
-log_type = input('Log type (Debug or Info): ')
-log_hash = input('Log hash: ')
-log_message = input('Log message: ')
-log_date = str(datetime.datetime.now())
-
-message_body = '{"hashValue": "%s", "message": "%s", "timestamp": "%s"}' % (log_hash, log_message, log_date)
+severities = ['Info', 'Debug']
 
 exchange_name = 'LogProcessingExchange'
 
-channel.exchange_declare(exchange='LogProcessingExchange', exchange_type='direct', durable=True)
-
-routing_key = 'processLog' + log_type
+scheduler = BlockingScheduler()
 
 
-channel.basic_publish(
-    exchange='LogProcessingExchange', routing_key=routing_key, body=message_body)
-print(" Sent %s:%s" % (routing_key, message_body))
-connection.close()
+def generate_message():
+    log_hash = str(random.getrandbits(128))
+    random_string = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=5))
+    log_message = f'{random_string} is a random string pretending to be a log'
+    log_date = str(datetime.datetime.now())
+    message_body = '{"hashValue": "%s", "message": "%s", "timestamp": "%s"}' % (log_hash, log_message, log_date)
+    return message_body
+
+
+@scheduler.scheduled_job(IntervalTrigger(seconds=2))
+def send_message():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    log_type = random.choice(severities)
+
+    message_body = generate_message()
+
+    channel.exchange_declare(exchange='LogProcessingExchange', exchange_type='direct', durable=True)
+
+    routing_key = 'processLog' + log_type
+
+    channel.basic_publish(
+        exchange='LogProcessingExchange', routing_key=routing_key, body=message_body)
+    print(" Sent %s:%s" % (routing_key, message_body))
+    connection.close()
+
+
+scheduler.start()
--- queueClient/processed_log_consumer.py
+++ queueClient/processed_log_consumer.py
@@ -1,29 +1,32 @@
 import pika
 
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
-
 exchange_name = "LogProcessedExchange"
-
-channel.exchange_declare(exchange=exchange_name, exchange_type='direct', durable=True)
 
 queue_name = "LogProcessedQueue"
 
-result = channel.queue_declare(queue=queue_name, durable=True)
-
-
-channel.queue_bind(exchange=exchange_name, queue=queue_name, routing_key='processedLog')
-
-print('Waiting for processed logs. To exit press Ctrl+C')
-
 
 def callback(ch, method, properties, body):
-    print(" [log] %s:%s" % (method.routing_key, body))
+    print(" [log] %s:%s" % (method.routing_key, body.decode()))
 
 
-channel.basic_consume(
-    queue=queue_name, on_message_callback=callback, auto_ack=True)
+def main():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
 
-channel.start_consuming()
+    channel.exchange_declare(exchange=exchange_name, exchange_type='direct', durable=True)
+
+    result = channel.queue_declare(queue=queue_name, durable=True)
+
+    channel.queue_bind(exchange=exchange_name, queue=queue_name, routing_key='processedLog')
+
+    channel.basic_consume(
+        queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+    print('Waiting for processed logs. To exit press Ctrl+C')
+
+    channel.start_consuming()
+
+
+main()
